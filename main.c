bc826c44aae0d48eb949a924caac00d5b90ceb16
eric <fakeemail@example.com>
2021-04-05 12:08:02 +0900

    Day 1 FINAL WORKING

--- main.c
+++ main.c
@@ -7,7 +7,11 @@
 #include "hUGEDriver.h"
 #include "sound.h"
 
-//this is the original
+extern const hUGESong_t songmario; //there is a var called twitchsong somewhere
+extern const hUGESong_t songwinner;
+extern const hUGESong_t songbaseball;
+unsigned char current_sfx = 0;
+unsigned char joystate;
 
 const char blankmap[1] = {0x00};
 UINT8 playerlocation[2];
@@ -15,15 +19,55 @@ UBYTE debug = 0;  //equals 1 by default (but not for tut?)
 UBYTE haskey = 0; //equals 1 by default (but not for tut?)
 UBYTE gamerunning;
 
-extern const hUGESong_t songmario; //there is a var called twitchsong somewhere
-extern const hUGESong_t songwinner;
-
+//defining a function
 void performantdelay(UINT8 numloops)
-{ //defining a function
+{
     UINT8 i;
     for (i = 0; i < numloops; i++)
     {
         wait_vbl_done(); //vbl_done = every time the screen is finished writing
+    }
+}
+
+UBYTE canplayermove(UINT8 newplayerx, UINT8 newplayery);       //forward declare
+void animatesprite(UINT8 spriteindex, INT8 movex, INT8 movey); //forward declare
+void movecheck()
+{
+    if (joypad() & J_LEFT)
+    {
+        if (canplayermove(playerlocation[0] - 8, playerlocation[1]))
+        {
+            playerlocation[0] -= 8;
+            animatesprite(0, -8, 0);
+        }
+    }
+
+    else if (joypad() & J_RIGHT)
+    {
+        if (canplayermove(playerlocation[0] + 8, playerlocation[1]))
+        {
+            playerlocation[0] += 8;
+            animatesprite(0, +8, 0);
+            // move_sprite(0, playerlocation[0], playerlocation[1]);
+        }
+    }
+    else if (joypad() & J_UP)
+    {
+        if (canplayermove(playerlocation[0], playerlocation[1] - 8))
+        {
+            playerlocation[1] -= 8;
+            animatesprite(0, 0, -8);
+            // move_sprite(0, playerlocation[0], playerlocation[1]);
+        }
+    }
+    else if (joypad() & J_DOWN)
+    {
+        if (canplayermove(playerlocation[0], playerlocation[1] + 8))
+        {
+            playerlocation[1] += 8;
+            animatesprite(0, 0, +8);
+            // move_sprite(0, playerlocation[0], playerlocation[1]);
+        }
     }
 }
 
@@ -51,11 +95,17 @@ UBYTE canplayermove(UINT8 newplayerx, UINT8 newplayery)
         //collect key
         result = 1; //allows you to walk into the previously inaccessable space
         haskey = 1;
+        sfx_play(SFX_5);
         set_bkg_tiles(1, 8, 1, 1, blankmap); //the tile at the key location will blank
     }
+    // else if (tileindexTL == 122)
+    // {
+    //     sfx_play(SFX_6);
+    // }
     else if (tileindexTL == 122 && haskey)
     {
         //open door
+        sfx_play(SFX_7);
         result = 1;
         set_bkg_tiles(2, 6, 1, 1, blankmap);
     }
@@ -63,6 +113,7 @@ UBYTE canplayermove(UINT8 newplayerx, UINT8 newplayery)
     if (tileindexTL == 201)
     {
         //open chest
+        sfx_play(SFX_3);
         result = 1;
         set_bkg_tiles(1, 10, 1, 1, blankmap);
     }
@@ -71,14 +122,13 @@ UBYTE canplayermove(UINT8 newplayerx, UINT8 newplayery)
         //finish game
         gamerunning = 0;
 
+        HIDE_SPRITES;
+        printf("\n \n \n \n \n \n \n \n \n      YOU WIN!");
+        result = 1;
         __critical
         {
             hUGE_init(&songwinner); //adding add_VBL(hUGE_dosound); twice actually speeds up the audio 2x
         }
-
-        HIDE_SPRITES;
-        printf("\n \n \n \n \n \n \n \n \n      YOU WIN!");
-        result = 1;
     }
 
     return result;
@@ -112,7 +162,7 @@ void main()
 
     __critical
     {
-        hUGE_init(&songmario);
+        hUGE_init(&songbaseball);
         add_VBL(hUGE_dosound);
     }
 
@@ -129,46 +179,65 @@ void main()
 
     while (gamerunning)
     {
+        //debugging feature
+        //  if(joypad() & J_A){
+        //     debug = (debug == 0 ? 1 : 0);
+        // }
 
-        if (joypad() & J_A)
-        {
-            debug = (debug == 0 ? 1 : 0);
-        }
-        if (joypad() & J_LEFT)
-        {
-            if (canplayermove(playerlocation[0] - 8, playerlocation[1]))
-            {
-                playerlocation[0] -= 8;
-                animatesprite(0, -8, 0);
-            }
-        }
-        else if (joypad() & J_RIGHT)
-        {
-            if (canplayermove(playerlocation[0] + 8, playerlocation[1]))
-            {
-                playerlocation[0] += 8;
-                animatesprite(0, +8, 0);
-                // move_sprite(0, playerlocation[0], playerlocation[1]);
-            }
-        }
-        else if (joypad() & J_UP)
-        {
-            if (canplayermove(playerlocation[0], playerlocation[1] - 8))
-            {
-                playerlocation[1] -= 8;
-                animatesprite(0, 0, -8);
-                // move_sprite(0, playerlocation[0], playerlocation[1]);
-            }
-        }
-        else if (joypad() & J_DOWN)
-        {
-            if (canplayermove(playerlocation[0], playerlocation[1] + 8))
-            {
-                playerlocation[1] += 8;
-                animatesprite(0, 0, +8);
-                // move_sprite(0, playerlocation[0], playerlocation[1]);
-            }
-        }
         performantdelay(6);
+
+        joystate = joypad();
+        // movecheck(); //will movecheck for every button press from here rather than below
+        switch (joystate)
+        {
+        case J_A:
+            // sfx_play(SFX_0);
+            movecheck();
+            waitpadup();
+            break;
+        case J_B:
+            // sfx_play(SFX_1);
+            movecheck();
+            waitpadup();
+            break;
+        case J_UP:
+            sfx_play(SFX_4);
+            movecheck();
+            // waitpadup();
+            performantdelay(2);
+            break;
+        case J_DOWN:
+            sfx_play(SFX_4);
+            movecheck();
+            // waitpadup();
+            performantdelay(2);
+            break;
+        case J_LEFT:
+            sfx_play(SFX_4);
+            movecheck();
+            // waitpadup();
+            performantdelay(2);
+            break;
+        case J_RIGHT:
+            sfx_play(SFX_4);
+            movecheck();
+            // waitpadup();
+            performantdelay(2);
+            break;
+        case J_START:
+            sfx_play(SFX_2);
+            movecheck();
+            waitpadup();
+            break;
+        case J_SELECT:
+            // sfx_play(SFX_7);
+            movecheck();
+            waitpadup();
+            break;
+        default:
+            break;
+        }
+        sfx_update();
+        wait_vbl_done();
     }
 }
